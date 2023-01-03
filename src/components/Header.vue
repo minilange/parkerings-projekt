0743236912ae37f0feb207542536caf114d736d9
Sch0u <Jonasschou@live.dk>
2023-01-03 14:15:11 +0100

    Basic Navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark ">
+    <div class="mt-4 container-fluid mx-4">
+        <a href="" class="navbar-brand fw-bold">EASIER<span class="text-secondary fw-bold">PARK</span></a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse navbar-right" id="navbarCollapse">
+            <div class="navbar-nav ms-auto gap-5 fw-bold">
+                <a href="#" class="nav-item nav-link text-white">PARK</a>
+                <a href="#" class="nav-item nav-link text-white">AREAS</a>
+                <a href="#" class="nav-item nav-link text-white">ABOUT US</a>
+            </div>
+        </div>
+    </div>
+  </nav>
+</template>
