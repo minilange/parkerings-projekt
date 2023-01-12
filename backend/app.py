e8437a9f2482cb316b36a66f51545d158417442c
Lange <martinlange2002@gmail.com>
2023-01-12 12:53:31 +0100

    detection progress

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 import os
+import re
+import cv2
 import json
+import easyocr
+import imutils
+import numpy as np
 from enum import Enum
 from datetime import datetime
 from hashlib import sha512
@@ -400,6 +405,51 @@ def parkings():
             areas, ["licenseplate", "userId", "areaId", "minutes", "price", "state", "timestamp"])
 
         return Response(json.dumps(formatted), ResponseCodes.success.value)
+
+
+@app.route("/api/detectLicenseplate/", methods=["POST"])
+def detect_licenseplate():
+
+    
+
+    image = None
+        
+    img  = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+    edged = cv2.Canny(bfilter, 30, 200)
+    
+    keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(keypoints)
+    # contours = sorted(contours, key=lambda x: len(cv2.approxPolyDP(x, 10, True)))[:50]
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:50]
+
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 10, True)
+
+        mask = np.zeros(gray.shape, np.uint8)
+        new_image = cv2.drawContours(mask, [approx], 0,255, -1)
+        new_image = cv2.bitwise_and(img, img, mask=mask)
+
+        (x,y) = np.where(mask==255)
+        (x1, y1) = (np.min(x), np.min(y))
+        (x2, y2) = (np.max(x), np.max(y))
+        cropped_image = gray[x1:x2+1, y1:y2+1]
+
+        reader = easyocr.Reader(['en'])
+        result = reader.readtext(cropped_image)
+
+        if len(result) != 0:
+            formatted = result[-1][-2].replace(" ", "")
+            if len(formatted) > 5 and any(char.isdigit() for char in formatted):
+                # print(len(approx))
+                break
+
+    plate = result[-1][-2].replace(" ", "")
+    regex = re.compile('[^a-zA-Z0-9]')
+
+    return regex.sub('', plate)
 
 
 def insert(query: str):
